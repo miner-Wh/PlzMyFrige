e8c0f362f317ba9e35dbdc34223b60eafadf25d2
minWH <whyuk7545@gmail.com>
2021-04-19 18:51:46 +0900

    중간 업데이트 4/19 병합

--- app/src/main/java/gachon/myclass/plzmyfrige/MainActivity.java
+++ app/src/main/java/gachon/myclass/plzmyfrige/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button1 = findViewById(R.id.goRecipe);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button2 = (Button)findViewById(R.id.main_frige);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), // 현재 화면의 제어권자
+                        MyFrige.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.main_buying);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        Buying.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
