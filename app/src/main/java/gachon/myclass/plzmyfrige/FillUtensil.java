c055e87a5567d8e9cf230957b3c3069343bf0408
EUIJEONGHONG <haa1224@gmail.com>
2021-04-21 07:29:47 +0900

    재료채우기,도구채우기 추가했습니다. FillFrige.java, FillUtensil.java, fillutensil.xml, fillfrige.xml, frige.xml 변경 frige_SearchAdapter.java, frige.listview.xml 추가 2021/04/21 홍의정

--- app/src/main/java/gachon/myclass/plzmyfrige/FillUtensil.java
+++ app/src/main/java/gachon/myclass/plzmyfrige/FillUtensil.java
@@ -1,19 +1,144 @@
 package gachon.myclass.plzmyfrige;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FillUtensil extends Fragment {
+    List<String> list;          // 재료 리스트
+    ListView listView;          // 검색을 보여줄 리스트변수
+    EditText editSearch;        // 검색어를 입력할 Input 창
+    frige_SearchAdapter adapter;      // 리스트뷰에 연결할 아답터
+    ArrayList<String> arraylist;
 
     @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fillutensil,container,false);
+    @Override
+    public void onCreate(Bundle savedInstancestate){
+        super.onCreate(savedInstancestate);
+    }
+    @Override
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fillutensil, container, false);
+
+        editSearch = (EditText) v.findViewById(R.id.editSearch);
+        listView = (ListView) v.findViewById(R.id.listView);
+
+
+
+
+        // 리스트를 생성한다.
+        list = new ArrayList<String>();
+
+        // 검색에 사용할 데이터을  저장
+        settingList();
+
+
+        arraylist = new ArrayList<String>();
+        arraylist.addAll(list);
+
+
+        adapter = new frige_SearchAdapter(list, getActivity());
+
+        // 리스트뷰에 아답터를 연결한다.
+        listView.setAdapter(adapter);
+
+
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // input창에 문자를 입력할때마다 호출된다.
+                // search 메소드를 호출한다.
+                String text = editSearch.getText().toString();
+                search(text);
+
+            }
+        });
+
+        return v;
+
+    }
+
+    // 검색을 수행하는 메소드
+    public void search(String charText) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        list.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+            list.addAll(arraylist);
+        }
+        // 문자 입력을 할때..
+        else
+        {
+            // 리스트의 모든 데이터를 검색한다.
+            for(int i = 0;i < arraylist.size(); i++)
+            {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist.get(i).toLowerCase().contains(charText))
+                {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    list.add(arraylist.get(i));
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private void settingList(){
+        list.add("후라이팬");
+        list.add("뒤집개");
+        list.add("집게");
+        list.add("가위");
+        list.add("식칼");
+        list.add("웍");
+        list.add("찜기");
+        list.add("전자레인지");
+        list.add("오븐");
+        list.add("석쇠");
+        list.add("가스레인지");
+        list.add("거품기");
+        list.add("도마");
+        list.add("에어프라이어");
+        list.add("키친타올");
+        list.add("국자");
+        list.add("감자칼");
+        list.add("냄비");
+        list.add("밀대");
+        list.add("채반");
+        list.add("믹싱 볼");
+        list.add("바구니");
+        list.add("고무장갑");
+        list.add("비닐장갑");
+        list.add("앞치마");
+        list.add("소스통");
+        list.add("거름망");
     }
 }
+
+
+
