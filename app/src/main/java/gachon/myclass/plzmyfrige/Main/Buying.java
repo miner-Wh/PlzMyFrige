89796ff0da92045ee02e666b33ff2affed5af4f8
soominjeong99 <79966717+soominjeong99@users.noreply.github.com>
2021-05-28 03:09:25 +0900

    로그인 성공, 장보기 ui 변경

--- app/src/main/java/gachon/myclass/plzmyfrige/Main/Buying.java
+++ app/src/main/java/gachon/myclass/plzmyfrige/Main/Buying.java
@@ -1,9 +1,11 @@
 package gachon.myclass.plzmyfrige.Main;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,33 +19,31 @@ public class Buying extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buying);
 
-        Button onlineMall = findViewById(R.id.onlineMall);
-        Button nearMall = findViewById(R.id.nearMall);
-        Button returnPage = findViewById(R.id.returnPage);
+        ImageButton onlineMall = findViewById(R.id.onlineMallButton);
+        ImageButton nearMall = findViewById(R.id.nearMallButton);
 
-        View.OnClickListener listener = new View.OnClickListener(){
-
-            public void onClick(View v)
-            {
-             switch (v.getId())
-             {
-                 case R.id.onlineMall:
-                     //온라인몰 버튼을 눌렀을 때의 처리
-                     Intent intent = new Intent(getApplicationContext(), OnlineMall.class); startActivity(intent);
-                     break;
-
-                 case R.id.nearMall:
-                     //주변몰 버튼을 눌렀을 때의 처리
-                     break;
-
-                 case R.id.returnPage:
-                     //돌아가기 버튼을 눌렀을 때의 처리
-                     Toast.makeText(getApplicationContext(),"돌아가기버튼이 눌렸어요", Toast.LENGTH_LONG).show();
-                     finish();
-                     break;
-             }
+        //온라인 몰로 이동
+        onlineMall.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(getApplicationContext(), OnlineMall.class);
+                startActivity(intent);
+                finish();
             }
-        };
+        });
+
+        //주변마트로 이동
+        nearMall.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+               // Intent intent = new Intent(getApplicationContext(), OnlineMall.class);
+               // startActivity(intent);
+                // finish();
+            }
+        });
+
 
     }
 }
