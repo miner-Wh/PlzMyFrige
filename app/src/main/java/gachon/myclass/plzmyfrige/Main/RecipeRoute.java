8ee087cb529d9d60a554814a9dad4cd300278015
minWH <whyuk7545@gmail.com>
2021-05-31 11:12:58 +0900

    레시피 루트 및 리스트 최종 변경

--- app/src/main/java/gachon/myclass/plzmyfrige/Main/RecipeRoute.java
+++ app/src/main/java/gachon/myclass/plzmyfrige/Main/RecipeRoute.java
@@ -21,6 +21,7 @@ public class RecipeRoute extends AppCompatActivity {
         Button buttonM = findViewById(R.id.meat);
         Button buttonS = findViewById(R.id.seaFood);
         Button buttonV = findViewById(R.id.vegetable);
+        Button buttonA = findViewById(R.id.resipeAll);
 
         buttonB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +34,7 @@ public class RecipeRoute extends AppCompatActivity {
         buttonM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Toast.makeText(getApplicationContext(),
-                       "고기고기고기", Toast.LENGTH_LONG).show();
+               //Toast.makeText(getApplicationContext(),"고기고기고기", Toast.LENGTH_LONG).show();
                 //finish();
                 Intent intent = new Intent(getApplicationContext(), RecipeList.class);
                 intent.putExtra("category",1);
@@ -44,8 +44,7 @@ public class RecipeRoute extends AppCompatActivity {
         buttonS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "해물해물해물", Toast.LENGTH_LONG).show();
+               // Toast.makeText(getApplicationContext(),"해물해물해물", Toast.LENGTH_LONG).show();
                 //finish();
                 Intent intent = new Intent(getApplicationContext(), RecipeList.class);
                 intent.putExtra("category",2);
@@ -55,11 +54,20 @@ public class RecipeRoute extends AppCompatActivity {
         buttonV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "야채채소야채", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(),"야채채소야채", Toast.LENGTH_LONG).show();
                 //finish();
                 Intent intent = new Intent(getApplicationContext(), RecipeList.class);
                 intent.putExtra("category",3);
+                startActivity(intent);
+            }
+        });
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"모든레시피", Toast.LENGTH_LONG).show();
+                //finish();
+                Intent intent = new Intent(getApplicationContext(), RecipeList.class);
+                intent.putExtra("category",0);
                 startActivity(intent);
             }
         });
